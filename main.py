d4004860f5ab94f3f9492973da98c3a2188e35cb
Angel Corredor <adcorredorm@unal.edu.co>
2021-12-01 10:11:26 -0500

    estructura general del juego

---
+++ main.py
@@ -0,0 +1,31 @@
+def bienvenida():
+    pass
+
+def ganador(tablero:list) -> bool:
+    pass
+
+def jugar(jugador_actual:str, tablero:list):
+    pass
+
+def alternar_jugador(jugador_actual:str) -> str:
+    pass
+
+def mensaje_final(tablero:list):
+    pass
+
+def main():
+    bienvenida()
+    jugador:str = 'X'
+    tablero:list = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+        ]
+    while not ganador(tablero):
+        jugar(jugador, tablero)
+        jugador = alternar_jugador(jugador)
+    mensaje_final(tablero)
+
+
+if __name__ == '__main__':
+    main()
